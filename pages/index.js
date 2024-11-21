3b646e4a5c88a8ff8c637d8142667b316ae33e39
gsnunes47 <gustavompn06@gmail.com>
2024-11-20 21:28:17 -0300

    adição de dependencias

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <nav className="flex justify-end gap-24">
+        <a>Sobre</a>
+        <a>Projetos</a>
+        <a>Contato</a>
+      </nav>     
+    </div>
+  );
+}
+
+export default App;
